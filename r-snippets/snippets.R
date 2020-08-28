d43e89edab3fb55144df086a7186b0e297935c3e
Kyle F. Butts <buttskyle96@gmail.com>
2020-08-28 16:09:39 -0600

    Added RStudio Snippets

---
+++ r-snippets/snippets.R
@@ -0,0 +1,24 @@
+snippet banner
+bannerCommenter::banner("${1:title}", bandChar = "-", fold= TRUE, rightSideHashes= 4, minHashes= 80)
+
+snippet endhead
+## -----------------------------------------------------------------------------
+
+snippet introblock
+`r paste0("## -----------------------------------------------------------------------------\n", 
+		  "## ", format(Sys.time(), "%Y-%m-%d"), "_\n## Kyle Butts, CU Boulder Economics \n",
+		  "## \n", "## \n", 
+		  "## -----------------------------------------------------------------------------", sep="")`
+
+snippet func
+function() {
+	# Display what the function is doing
+	usethis::ui_todo("")
+	cat("\n")
+	
+	
+	
+	# Display what the function successfully finished
+	cat("\n")
+	usethis::ui_done("")
+}	
